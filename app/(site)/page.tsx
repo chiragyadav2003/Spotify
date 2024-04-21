d4ed8a55727c21828ce88231546707b4e8da19de
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-22 02:04:22 +0530

    initial styling setup

---
+++ app/(site)/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="h-full flex justify-center items-center text-green-500 text-4xl font-bold">
+      Spotify App from (site)
+    </div>
+  );
+}
