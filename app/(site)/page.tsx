9a02e989f903d892b73f598abe68fb9935c434ee
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-22 17:21:00 +0530

    finish layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,38 @@
+import { Header } from "@/components/header";
+import { ListItem } from "@/components/listItem";
+import likedImage from "@/public/images/liked.jpeg"
 
 export default function Home() {
   return (
-    <div className="h-full flex justify-center items-center text-green-500 text-4xl font-bold">
-      Spotify App from (site)
+    <div className=" bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      {/* header */}
+      <Header>
+        <div className="mb-2">
+          <h1 className=" text-3xl text-white font-semibold">
+            Welcome back
+          </h1>
+          <div
+            className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4"
+          >
+            <ListItem
+              image={likedImage.src}
+              name="Liked Songs"
+              href="liked"
+            />
+          </div>
+        </div>
+      </Header>
+      {/* new songs */}
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className=" text-2xl font-semibold text-white">
+            Newest songs
+          </h1>
+        </div>
+        <div>
+          List of songs !
+        </div>
+      </div>
     </div>
   );
 }
---
+++ components/listItem.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import Image from "next/image";
+import { useRouter } from "next/navigation"
+import { FaPlay } from "react-icons/fa"
+
+interface ListItemProps {
+    image: string;
+    name: string;
+    href: string
+}
+
+export function ListItem({ image, name, href }: ListItemProps) {
+    const router = useRouter()
+    const onClick = () => {
+        // add authentication before push
+        router.push(href)
+    }
+    return (
+        <button
+            onClick={onClick}
+            className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
+        >
+            <div className="relative min-h-[64px] min-w-[64px]">
+                <Image
+                    className=" object-cover"
+                    alt="image"
+                    fill
+                    src={image}
+                />
+            </div>
+            <p
+                className=" truncate font-medium py-5"
+            >
+                {name}
+            </p>
+            {/* this will be visible only when we hover on it */}
+            <div
+                className=" absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110"
+            >
+                <FaPlay className=" text-black" />
+            </div>
+        </button>
+    )
+}
