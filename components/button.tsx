5269809bedfc176533d6c8ccfd5e4ce45f90dcba
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-23 02:21:27 +0530

    toast for better mesages

--- components/button.tsx
+++ components/button.tsx
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 
 export function Button({ children, className, onClick }: { children: React.ReactNode, className?: string, onClick: () => void }) {
     return (
-        <button onClick={onClick} className={twMerge("w-full rounded-full bg-green-500 border border-transparent px-6 py-2 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition", className)}>
+        <button onClick={onClick} className={twMerge("w-full rounded-full bg-green-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition", className)}>
             {children}
         </button>
     )
