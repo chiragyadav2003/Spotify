92982fa3d6ef45c93d88e5a9e1d265b5fda9d9e0
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-24 02:53:50 +0530

    adding in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/userProvider";
 import ModalProvider from "@/providers/modalProvider";
 import ToasterProvider from "@/providers/toasterProvider";
+import getSongsByUserId from "@/actions/getSongsByUserId";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -14,11 +15,17 @@ export const metadata: Metadata = {
   description: "A new way to listen to music",
 };
 
-export default function RootLayout({
+//we do not want this layout to be cached
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const userSongs = await getSongsByUserId()
+
   return (
     <html lang="en">
       <body className={font.className}>
@@ -26,7 +33,7 @@ export default function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
-            <Sidebar>
+            <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
           </UserProvider>
