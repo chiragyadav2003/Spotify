45b4f502d6a87ba474114e97f10323f82327572f
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-23 19:40:13 +0530

    generic button

--- components/button.tsx
+++ components/button.tsx
@@ -1,9 +1,30 @@
 import { twMerge } from "tailwind-merge";
 
+// interface ButtonProps {
+//     children: React.ReactNode;
+//     className?: string;
+//     onClick?: () => void;
+//     disabled?: boolean;
+//     type?: string;
+// }
 
-export function Button({ children, className, onClick }: { children: React.ReactNode, className?: string, onClick: () => void }) {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+    className?: string;
+}
+
+export function Button({
+    children,
+    className,
+    type = "button",// Defaulting to 'button' to avoid unintentional submits
+    ...props // Spread remaining props to handle onClick, disabled, etc.
+}: ButtonProps) {
     return (
-        <button onClick={onClick} className={twMerge("w-full rounded-full bg-green-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition", className)}>
+        <button
+            type={type}
+            className={twMerge("w-full rounded-full bg-green-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition", className)}
+            {...props}
+        >
             {children}
         </button>
     )
