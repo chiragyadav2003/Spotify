271035c09acea63fb7db036b19b56f1dff17a15e
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-24 07:07:31 +0530

    searched content and search page

---
+++ app/search/_components/SearchContent.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import MediaItem from "@/components/mediaItem"
+import { Song } from "@/types"
+
+
+function SearchContent({ songs }: { songs: Song[] }) {
+
+    if (songs.length === 0) {
+        return (
+            <div
+                className=" flex flex-col gap-y-2 w-full px-6 text-neutral-400"
+            >
+                No song found
+            </div>
+        )
+    }
+
+    return (
+        <div className=" flex flex-col gap-y-2 w-full px-6 ">
+
+            {
+                songs.map((song) => (
+                    <div
+                        key={song.id}
+                        className="flex items-center gap-x-4 w-full"
+                    >
+                        <div className=" flex-1">
+                            <MediaItem
+                                data={song}
+                                onClick={() => { }}
+                            />
+                        </div>
+                        {/* TODO -add liked button */}
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default SearchContent
---
+++ app/search/page.tsx
@@ -0,0 +1,32 @@
+import getSongsByTitle from "@/actions/getSongsByTitle"
+import { Header } from "@/components/header"
+import SearchInput from "@/components/searchInput"
+import SearchContent from "./_components/SearchContent"
+
+interface SearchProps {
+    searchParams: {
+        title: string
+    }
+}
+
+const SearchPage = async ({ searchParams }: SearchProps) => {
+    const songs = await getSongsByTitle(searchParams?.title)
+
+    return (
+        <div
+            className=" bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto"
+        >
+            <Header className=" from-bg-bg-neutral-900">
+                <div className="mb-2 flex flex-col gap-y-6">
+                    <h1 className="text-white text-3xl font-semibold">
+                        Search
+                    </h1>
+                    <SearchInput />
+                </div>
+            </Header>
+            <SearchContent songs={songs} />
+        </div>
+    )
+}
+
+export default SearchPage;
