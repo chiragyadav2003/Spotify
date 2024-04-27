8c579981ad56e50333c85348950ccfda61d2fa50
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-04-27 07:00:25 +0530

    loading file

---
+++ app/(site)/loading.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Box } from "@/components/box"
+import { BounceLoader } from "react-spinners"
+
+function Loading() {
+    return (
+        <Box
+            className="h-full w-full flex items-center justify-center bg-neutral-400"
+        >
+            <BounceLoader color="#22c55e" size={40} />
+        </Box>
+    )
+}
+
+export default Loading
